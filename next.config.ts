2fdd72350692c9821de02ce77acbff49792474ff
Nobubele Mpande <nobubele.mpande@dma.co>
2026-04-27 00:47:46 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/mathsstudioswebsite",
-  assetPrefix: "/mathsstudioswebsite/",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
   images: {
     unoptimized: true,
     dangerouslyAllowSVG: true,
